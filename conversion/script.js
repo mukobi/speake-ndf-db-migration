d2c28704c3da73c7894af43a0553a788eed90a64
Mukobi <gabrielmukobi@gmail.com>
2018-06-11 17:55:47 -0700

    Setup basic connection to server

--- conversion/script.js
+++ conversion/script.js
@@ -1,1 +1,32 @@
-console.log("test")
+// Postgres constants-change as needed for local db:
+const USERNAME = "postgres"
+const PASSWORD = "postgres"
+const HOST = 'localhost'
+const DATABASE = "speake_postgres_db"
+
+// pg module is required
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+    user: USERNAME,
+    host: HOST,
+    database: DATABASE,
+    password: PASSWORD
+})
+
+async function main() {
+    await client.connect()
+
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
+}
+
+(async ()=>{
+    try{
+      var result = await main();
+      console.log(result);
+    }catch(e){
+      console.log(e)
+    }
+})()
